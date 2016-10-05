9eedd358d0e6cdfdd071c0887df66b9ac323874e
Magno Faustino <magno2004@live.com>
2016-10-05 20:34:17 -0300

    referente a criação da inserção de contas a pagar.

---
+++ processa_pagamento.php
@@ -0,0 +1,41 @@
+
+
+<?php
+
+require_once "class_pagamento.php"; 
+$con = mysql_connect("localhost", "root", "sos101os") or die ("erro ao conectar ao BD");
+$nome_aluno =$_GET['nome'];
+$cpf = $_GET['descricao'];
+$curso = $_GET['telefone']; 
+
+if($con){
+ echo("banco conectado com sucesso");   
+}
+
+
+ $s= mysql_select_db("mm") or die ("erro ao selecionar o BD PROJETO BAR");
+if($s){
+
+echo("banco selecionado com sucesso");
+}
+
+$prd=new Aluno();
+$prd->setNome($nome_aluno);
+$prd->setDescricao($cpf);
+$prd->setTelefone($curso);
+
+
+$result = mysql_query("insert into fornecedor(nome, descricao, telefone) values ('".$prd->getNome()."', '".$prd->getDescricao()."', '".$prd->getTelefone()."')") or die ("erro ao inserir os dados no bando");
+
+
+if($result){
+    echo("dados inseridos com sucesso");
+    
+}
+
+
+?>
+
+
+
+   
