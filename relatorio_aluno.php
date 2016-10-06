57bcc73b664df3100c46c531a36d091a1302f534
Sandro <Sandro@192.168.0.3>
2016-10-05 22:45:27 -0300

    Ajuste de nomenclaturas

--- relatorio_aluno.php
+++ relatorio_aluno.php
@@ -3,37 +3,21 @@ include "inicial.php";
 ?>
 <html>
 <meta charset="UTF-8"/>
-<head>
-<title>Cadastrar Produto</title>
-    
-
-    
-    
-</head>
-    
+<head> <title>Relatório de Alunos</title> </head>  
 <body>
-
-<h1>informações sobre o aluno</h1>
-
+<h1>Informações sobre o aluno</h1>
 <?php
-
-
 
 $con = mysql_connect("localhost", "root", "sos101os") or die ("erro ao conectar ao BD");
 
  //função seleciona banco de dados.
     $seleciona_banco = mysql_select_db("escola") or die ("erro ao selecionar o BD PROJETO BAR");
 
-
 $resoltbusca = mysql_query("select * from curso") or die ("erro executar o select");
-   
-
-
 
 if($con){
      echo("conecção feita");
      }
-
 
 ?>
     
@@ -44,26 +28,22 @@ if($con){
     
     <tr>
         
-        
-        <td>aluno</td>
-        <td>cpf</td>
+        <td>Aluno</td>
+        <td>Cpf</td>
        
         </tr>
     
     <?php while($ras = mysql_fetch_array($resoltadobusca)){?> 
-   <?php $n = $ras['numero'];
+      <?php $n = $ras['numero'];
                                                            ?>
         <tr>
         
         <td><?php echo $ras['nome_aluno']; ?></td>
         <td><?php echo $ras['cpf']; ?></td>
        
-    </tr>
-   <?php } ?>
+           </tr>
+        <?php } ?>
     </table><br><br>
-    
-   
- 
+  
 </body>
-
 </html> 
