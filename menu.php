8a24e9ac46eb4d7d57c050f88d8679bba8290e1f
Sandro <Sandro@192.168.0.3>
2016-10-05 21:53:13 -0300

    Teste de novo commit

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 
 <a href="cadastro_funcionario.php">cadastro de funcionario</a><br>
-<a href="cadastro-forcededor.php" >cadastro de Fornecedor</a><br>
+<a href="cadastro_forcededor.php" >cadastro de Fornecedor</a><br>
 <a href="cadastro_aluno.php" >cadastro de aluno</a><br>
 <a href="cadastro_pagamento.php" >cadastro de contas a pagar</a><br>
 <a href="relatorio_aluno.php" >relatorio alunos</a><br>
