34c4d5c69c8bae306ba98827229a39237ce8fac4
Magno Faustino <magno2004@live.com>
2016-10-05 22:38:50 -0300

    arquivo referente a select dos alunos

---
+++ relatorio_aluno.php
@@ -0,0 +1,69 @@
+<?php
+include "inicial.php";
+?>
+<html>
+<meta charset="UTF-8"/>
+<head>
+<title>Cadastrar Produto</title>
+    
+
+    
+    
+</head>
+    
+<body>
+
+<h1>informações sobre o aluno</h1>
+
+<?php
+
+
+
+$con = mysql_connect("localhost", "root", "sos101os") or die ("erro ao conectar ao BD");
+
+ //função seleciona banco de dados.
+    $seleciona_banco = mysql_select_db("escola") or die ("erro ao selecionar o BD PROJETO BAR");
+
+
+$resoltbusca = mysql_query("select * from curso") or die ("erro executar o select");
+   
+
+
+
+if($con){
+     echo("conecção feita");
+     }
+
+
+?>
+    
+     <?php  $resoltadobusca = mysql_query("select * from aluno") or die ("erro executar o select");
+   
+   ?> 
+    <table border='2'>
+    
+    <tr>
+        
+        
+        <td>aluno</td>
+        <td>cpf</td>
+       
+        </tr>
+    
+    <?php while($ras = mysql_fetch_array($resoltadobusca)){?> 
+   <?php $n = $ras['numero'];
+                                                           ?>
+        <tr>
+        
+        <td><?php echo $ras['nome_aluno']; ?></td>
+        <td><?php echo $ras['cpf']; ?></td>
+       
+    </tr>
+   <?php } ?>
+    </table><br><br>
+    
+   
+ 
+</body>
+
+</html> 
