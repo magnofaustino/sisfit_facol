d63a17b1857c3cf257a09269bf0fd737910a1c51
Sandro <Sandro@192.168.0.3>
2016-10-18 23:27:31 -0300

    Menu com Bootstrap

--- menu.php
+++ menu.php
@@ -1,8 +1,45 @@
+<html>
+  
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <link href="css/bootstrap.css"
+    rel="stylesheet" type="text/css">
+  </head>
+  
+  <body>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <ul class="nav nav-pills">
+              <li  class="active">
+                <a href="#">Sisfit</a>
+              </li>
+              <li>
+                <a href="cadastro_funcionario.php">Cadastro de Funcionario</a>
+              </li>
+              <li>
+                  <a href="cadastro_forcededor.php">Cadastro de Fornecedor</a>
+              </li>
+              <li>
+                  <a href="cadastro_aluno.php">Cadastro de Aluno</a>
+              </li>
+              <li>
+                  <a href="cadastro_atividades.php">Cadastro de Atividades</a>
+              </li>
+              <li>
+                  <a href="cadastro_pagamentos.php">Cadastro de Contas a Pagar</a>
+              </li>
+              <li>
+                  <a href="relatorio_aluno.php">Relatorio Alunos</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
 
-<a href="cadastro_funcionario.php">Cadastro de Funcionario</a><br>
-<a href="cadastro_forcededor.php" >Cadastro de Fornecedor</a><br>
-<a href="cadastro_aluno.php" >Cadastro de Aluno</a><br>
-<a href="cadastro_atividades.php" >Cadastro de Atividades</a><br>
-<a href="cadastro_pagamento.php" >Cadastro de Contas a Pagar</a><br>
-<a href="relatorio_aluno.php" >Relatorio Alunos</a><br>
-
+</html>
