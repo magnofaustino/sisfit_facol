e10ccd83054c096b38adccafc32c85bd18403a42
Sandro <Sandro@192.168.0.3>
2016-10-18 23:02:04 -0300

    Arquivos Bootstrap.

--- conexao.php
+++ conexao.php
@@ -2,13 +2,13 @@
 $conexao = "localhost";
 $user = "root";
 $senha = "sos101os";
-$alias = "escola"; 
+$alias = "sisfit"; 
      
 $mysqli = new mysqli($conexao,$user,$senha,$alias) or die ("Erro ao conectar ao DB");
-
+    
 if($mysqli){
     echo ("conectado com sucesso");
     
 }
 
-  ?>   
+?>   
