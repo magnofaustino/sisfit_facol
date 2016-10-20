3737e5d68ef23d1d040484f840df068377f62619
Magno Faustino <magno2004@live.com>
2016-10-20 14:16:23 -0300

    ainda esta com um erro no insert, so está inserindo 2 informaçoes no banco, tenho que rever o que esta acontecendo ainda

--- processa_aluno.php
+++ processa_aluno.php
@@ -18,15 +18,39 @@ $curso = $_GET['curso'];
 $prd=new Aluno();
 $prd->setNome($nome);
 $prd->setCpf($cpf);
-$prd->setCurso($curso);
+$prd->setRg($rg);
+$prd->setSexo($sexo);
+$prd->setProfissao($profissao);
+$prd->setOrgao_emissor($orgao_emissor);
+$prd->setDt_nascimento($dt_nascimento);
+$prd->setEndereco($endereco);
+$prd->setBairro($bairro);
+$prd->setEstado($estado);
+$prd->setNumero($numero);
+$prd->setCep($cep);
+$prd->setUf_naturalidade($uf_naturalidade);
+$prd->setNaturalidade($naturalidade);
+$prd->setNacionalidade($nacionalidade);
+$prd->setDeficiente($deficiente);
+$prd->setObs_deficiente($obs_deficiente);
+$prd->setEmail($email);
+$prd->setTelefone($telefone);
+$prd->setPlano($plano);
+$prd->setBolsa($bolsa);
+$prd->setTipo_bolsa($tipo_bolsa);
+$prd->setHorario($horario);
+$prd->setDt_cadastro($dt_cadastro);
 
 
 
-      // $sql = "INSERT INTO aluno(nome_aluno, cpf, curso)
-//VALUES ('".$prd->getNome()."', '".$prd->getCpf()."', '".$prd->getCurso()."')";
+  
 
 
-$sql = "INSERT INTO aluno(nome_aluno, cpf, curso) VALUES ('".$prd->getNome()."', '".$prd->getCpf()."', '".$prd->getCurso()."')";
+
+
+
+
+$sql = "INSERT INTO funcionario(nome, cpf, rg, telerone, matricula, data_emissao, data_nascimento, sexo, estado_sivil, nacionalidade, uf, naturalidade, deficiente, obs, nome_pai, nome_mae, apelido, funcao, n_carteira, uf_carteira, turno_trabalho, tipo_trabalho) VALUES ('".$prd->getNome()."', '".$prd->getCpf()."', '".$prd->getRg()."', '".$prd->getSexo()."', '".$prd->getProfissao()."', '".$prd->getOrgao_emissor()."', '".$prd->getDt_nascimento()."', '".$prd->getEndereco()."', '".$prd->getBairro()."', '".$prd->getEstado()."', '".$prd->getNumero()."', '".$prd->getCep()."', '".$prd->getUf_naturalidade()."', '".$prd->getNaturalidade()."', '".$prd->getNacionalidade()."', '".$prd->getDeficiente()."', '".$prd->getObs_deficiente()."', '".$prd->getEmail()."', '".$prd->getTelefone()."', '".$prd->getPlano()."', '".$prd->getBolsa()."', '".$prd->getTipo_bolsa()."')";
 
 
 
