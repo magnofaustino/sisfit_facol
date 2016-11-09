d2b4510612259f97f307b400d9f0700aa519b135
Magno Faustino <magno2004@live.com>
2016-11-09 11:37:05 -0300

    cadastro de funcionaio inserindo com sucesso, sql no arquivo no drive

--- class_funcionario.php
+++ class_funcionario.php
@@ -1,88 +1,97 @@
 <?php
+
+
+
 class Funcionario{
 
     
-public $nome;
-public $cpf;
-public $rg;
-public $email;
-public $telefone;
-public $matricula;
-public $admissao;
-public $nascimento;
-public $sexo;
-public $estadocivil;
-public $nacionalidade;
-public $ufnaturalidade;
-public $naturalidade;
-public $deficiente;
-public $obsdeficiente;
-public $nomepai;
-public $nomemae;
-public $apelido;
-public $funcao;
-public $numcarteira;
-public $seriecarteira;
-public $ufcarteira;
-public $turnotrab;
-public $tipotrab;
-
+public $nome_1;
+public $nome_2;
+public $nome_3;
+public $nome_4;
+public $nome_5;
+public $nome_6;
+public $nome_7;
+public $nome_8;
+public $nome_9;
+public $nome_10;
+public $nome_11;
+public $nome_12;
+public $nome_13;
+public $nome_14;
+public $nome_15;
+public $nome_16;
+public $nome_17;
+public $nome_18;
+public $nome_19;
+public $nome_20;
+public $nome_21;
+public $nome_22;
+public $nome_23;
+public $nome_24;
+    
     function _construct()
 	{		
 				
 	}
     
-    	public function getNome(){return $this->nome;}
-        public function getCpf(){return $this->cpf;}
-        public function getRg(){return $this->rg;}
-        public function getTelefone(){return $this->telefone;}
-        public function getMatricula(){return $this->matricula;}
-        public function getAdmissao(){return $this->admissao;}
-        public function getNascimento(){return $this->nascimento;}
-        public function getSexo(){return $this->sexo;}
-        public function getEstadocivil(){return $this->estadocivil;}
-        public function getNacionalidade(){return $this->nacionalidade;}
-        public function getUfnaturalidade(){return $this->ufnaturalidade;}
-        public function getNaturalidade(){return $this->naturalidade;}
-        public function getDeficiente(){return $this->deficiente;}
-        public function getObsdeficiente(){return $this->obsdeficiente;}
-        public function getNomepai(){return $this->nomepai;}
-        public function getNomemae(){return $this->nomemae;}
-        public function getApelido(){return $this->apelido;}
-        public function getFuncao(){return $this->funcao;}
-        public function getNumcarteira(){return $this->numcarteira;}
-        public function getSeriecarteira(){return $this->seriecarteira;}
-        public function getUfcarteira(){return $this->ufcarteira;}
-        public function getTurnotrab(){return $this->turnotrab;}
-        public function getTipotrab(){return $this->tipotrab;}
+    	public function getNome_1(){return $this->nome_1;}
+        public function getNome_2(){return $this->nome_2;}
+        public function getNome_3(){return $this->nome_3;}
+        public function getNome_4(){return $this->nome_4;}
+        public function getNome_5(){return $this->nome_5;}    
+        public function getNome_6(){return $this->nome_6;}
+        public function getNome_7(){return $this->nome_7;}
+        public function getNome_8(){return $this->nome_8;}
+        public function getNome_9(){return $this->nome_9;}
+        public function getNome_10(){return $this->nome_10;}
+        public function getNome_11(){return $this->nome_11;}
+        public function getNome_12(){return $this->nome_12;}
+        public function getNome_13(){return $this->nome_13;}
+        public function getNome_14(){return $this->nome_14;}
+        public function getNome_15(){return $this->nome_15;}
+        public function getNome_16(){return $this->nome_16;}
+        public function getNome_17(){return $this->nome_17;}
+        public function getNome_18(){return $this->nome_18;}
+        
+        public function getNome_19(){return $this->nome_19;}
+        public function getNome_20(){return $this->nome_20;}
+        public function getNome_21(){return $this->nome_21;}
+        public function getNome_22(){return $this->nome_22;}
+        public function getNome_23(){return $this->nome_23;}
+        public function getNome_24(){return $this->nome_24;}
+       
     
     
+        public function setNome_1($nome_1){$this->nome_1=$nome_1;}
+        public function setNome_2($nome_2){$this->nome_2=$nome_2;}
+        public function setNome_3($nome_3){$this->nome_3=$nome_3;}
+        public function setNome_4($nome_4){$this->nome_4=$nome_4;}
+        public function setNome_5($nome_5){$this->nome_5=$nome_5;}
+        public function setNome_6($nome_6){$this->nome_6=$nome_6;}
+        public function setNome_7($nome_7){$this->nome_7=$nome_7;}
+        public function setNome_8($nome_8){$this->nome_8=$nome_8;}
+        public function setNome_9($nome_9){$this->nome_9=$nome_9;}
+    
+        public function setNome_10($nome_10){$this->nome_10=$nome_10;}
+        public function setNome_11($nome_11){$this->nome_11=$nome_11;}
+        public function setNome_12($nome_12){$this->nome_12=$nome_12;}
+        public function setNome_13($nome_13){$this->nome_13=$nome_13;}
+        
+    
+        public function setNome_14($nome_14){$this->nome_14=$nome_14;}
+        public function setNome_15($nome_15){$this->nome_15=$nome_15;}
+        public function setNome_16($nome_16){$this->nome_16=$nome_16;}
+        public function setNome_17($nome_17){$this->nome_17=$nome_17;}
+        public function setNome_18($nome_18){$this->nome_18=$nome_18;}
     
     
-        public function setNome($nome){$this->nome=$nome;}
-        public function setCpf($cpf){$this->cpf=$cpf;}
-        public function setRg($rg){$this->rg=$rg;}
-        public function setTelefone($telefone){$this->telefone=$telefone;}
-        public function setMatricula($matricula){$this->rg=$matricula;}
-        public function setAdmissao($admissao){$this->telefone=$admissao;}
-        public function setNascimento(){return $this->nascimento;}
-        public function setSexo(){return $this->sexo;}
-        public function setEstadocivil(){return $this->estadocivil;}
-        public function setNacionalidade(){return $this->nacionalidade;}
-        public function setUfnaturalidade(){return $this->ufnaturalidade;}
-        public function setNaturalidade(){return $this->naturalidade;}
-        public function setDeficiente(){return $this->deficiente;}
-        public function setObsdeficiente(){return $this->obsdeficiente;}
-        public function setNomepai(){return $this->nomepai;}
-        public function setNomemae(){return $this->nomemae;}
-        public function setApelido(){return $this->apelido;}
-        public function setFuncao(){return $this->funcao;}
-        public function setNumcarteira(){return $this->numcarteira;}
-        public function setSeriecarteira(){return $this->seriecarteira;}
-        public function setUfcarteira(){return $this->ufcarteira;}
-        public function setTurnotrab(){return $this->turnotrab;}
-        public function setTipotrab(){return $this->tipotrab;}
-    
+        public function setNome_19($nome_19){$this->nome_19=$nome_19;}
+        public function setNome_20($nome_20){$this->nome_20=$nome_20;}
+        public function setNome_21($nome_21){$this->nome_21=$nome_21;}
+        public function setNome_22($nome_22){$this->nome_22=$nome_22;}
+        public function setNome_23($nome_23){$this->nome_23=$nome_23;}
+        public function setNome_24($nome_24){$this->nome_24=$nome_24;}
     
 }
 ?>
--- processa_funcionario.php
+++ processa_funcionario.php
@@ -12,53 +12,71 @@ require_once "class_funcionario.php";
 $nome_funcionario = $_GET['nome'];
 $cpf_funcionario = $_GET['cpf'];
 $rg_funcionario = $_GET['rg']; 
-$telefone_funcionario = $_GET['telefone'];
-$matricula_funcionario = $_GET['matricula']; 
-$admissao_funcionario = $_GET['admissao'];
-$nascimento_funcionario = $_GET['nascimento'];
-$sexo_funcionario = $_GET['sexo'];
-$estadocivil_funcionario = $_GET['estadocivil'];
-$nacionalidade_funcionario = $_GET['nacionalidade'];
-$ufnaturalidade_funcionario = $_GET['ufnaturalidade'];
-$naturalidade_funcionario = $_GET['naturalidade'];
-$deficiente_funcionario = $_GET['deficiente'];
-$obsdeficiente_funcionario = $_GET['obsdeficiente'];
-$nomepai_funcionario = $_GET['nomepai'];
-$nomemae_funcionario = $_GET['nomemae'];
-$apelido_funcionario = $_GET['apelido'];
-$funcao_funcionario = $_GET['funcao'];
-$numcarteira_funcionario = $_GET['numcarteira'];
-$seriecarteira_funcionario = $_GET['seriecarteira'];
-$ufcarteira_funcionario = $_GET['ufcarteira'];
-$turnotrab_funcionario = $_GET['turnotrab'];
-$tipotrab_funcionario = $_GET['tipotrab'];
+$nascimento = $_GET['nascimento'];
+$telefone = $_GET['telefone']; 
+$matricula = $_GET['matricula'];
+$estadocivil = $_GET['estadocivil'];
+$nacionalidade = $_GET['nacionalidade'];
+$naturalidade = $_GET['naturalidade'];
+$ufnaturalidade = $_GET['ufnaturalidade'];
+$admissao = $_GET['admissao'];
+$saida = $_GET['saida'];
+$sexo = $_GET['sexo'];
+$deficiente = $_GET['deficiente'];
+$obsdeficiente = $_GET['obsdeficiente'];
+$nomepai = $_GET['nomepai'];
+$nomemae = $_GET['nomemae'];
+$apelido = $_GET['apelido'];
+$funcao= $_GET['funcao'];
+$numcarteira = $_GET['numcarteira'];
+$seriecarteira = $_GET['seriecarteira'];
+$uf_naturalidade= $_GET['uf_naturalidade'];
+$turnotrab = $_GET['turnotrab'];
+$tipotrab = $_GET['tipotrab'];
 
 
 $prd=new Funcionario();
-$prd->setNome($nome);
-$prd->setCpf($cpf);
-$prd->setRg($rg);
-$prd->setTelefone($telefone);
-$prd->setMatricula($matricula);
-$prd->setAdmissao($admissao);
-$prd->setNascimento($nascimento);
-$prd->setSexo($sexo);
-$prd->setEstadocivil($estadocivil);
-$prd->setNacionalidade($nacionalidade);
-$prd->setUfnaturalidade($ufnaturalidade);
-$prd->setNaturalidade($naturalidade);
-$prd->setDeficiente($deficiente);
-$prd->setObsdeficiente($obsdeficiente);
-$prd->setNomepai($nomepai);
-$prd->setNomemae($nomemae);
-$prd->setApelido($apelido);
-$prd->setFuncao($funcao);
-$prd->setNumcarteira($numcarteira);
-$prd->setSeriecarteira($seriecarteira);
-$prd->setUfcarteira($ufcarteira);
-$prd->setTurnotrab($turnotrab);
-$prd->setTipotrab($tipotrab);
+$prd->setNome_1($nome);
+$prd->setNome_2($cpf);
+$prd->setNome_3($rg);
+$prd->setNome_4($nascimento);
+$prd->setNome_5($telefone);
+$prd->setNome_6($matricula);
+$prd->setNome_7($estadocivil);
+$prd->setNome_8($nacionalidade);
+$prd->setNome_9($naturalidade);
+$prd->setNome_10($ufnaturalidade);
+$prd->setNome_11($admissao);
+$prd->setNome_12($saida);
+$prd->setNome_13($sexo);
+$prd->setNome_14($deficiente);
+
+$prd->setNome_15($obsdeficiente);
+$prd->setNome_16($nomepai);
+$prd->setNome_17($nomemae);
+$prd->setNome_18($apelido);
+$prd->setNome_19($funcao);
 
 
-$result = mysql_query("insert into funcionario(nome, cpf, rg, telefone, matricula, admissao) values ('".$prd->getNome()."', '".$prd->getCpf()."', '".$prd->getRG()."', '".$prd->getMatricula()."', '".$prd->getAdmissao()."', '".$prd->getTelefone()."')") or die ("Erro ao inserir os dados no DB");
+$prd->setNome_20($numcarteira);
+$prd->setNome_21($seriecarteira);
+$prd->setNome_22($uf_naturalidade);
+$prd->setNome_23($turnotrab);
+$prd->setNome_24($tipotrab);
+
+
+
+
+
+$sql = "INSERT INTO funcionario(nome, cpf, rg, nascimento,telefone, matricula, estadocivil, nacionalidade, naturalidade, ufnaturalidade, admissao, saida, sexo, deficiente, obsdeficiente,  nomepai, nomemae, apelido, funcao, numcarteira, seriecarteira, uf_naturalidade, turnotrab, tipotrab) VALUES ('".$prd->getNome_1()."', '".$prd->getNome_2()."', '".$prd->getNome_3()."', '".$prd->getNome_4()."', '".$prd->getNome_5()."', '".$prd->getNome_6()."', '".$prd->getNome_7()."', '".$prd->getNome_8()."', '".$prd->getNome_9()."', '".$prd->getNome_10()."', '".$prd->getNome_11()."', '".$prd->getNome_12()."', '".$prd->getNome_13()."', '".$prd->getNome_14()."' , '".$prd->getNome_15()."', '".$prd->getNome_16()."', '".$prd->getNome_17()."' , '".$prd->getNome_18()."', '".$prd->getNome_19()."', '".$prd->getNome_20()."' , '".$prd->getNome_21()."', '".$prd->getNome_22()."', '".$prd->getNome_23()."' , '".$prd->getNome_24()."')";
+
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "New records created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
