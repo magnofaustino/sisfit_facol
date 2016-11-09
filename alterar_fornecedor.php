ea7007eda12f38331cb557d12bf21fdd02dade9f
Magno Faustino <magno2004@live.com>
2016-11-09 14:44:48 -0300

    cadastro de fornecedor funcionando corretamente, editando corretamente e atualizando corretamente, sql no drive

--- alterar_fornecedor.php
+++ alterar_fornecedor.php
@@ -1,7 +1,38 @@
 
 <?php
-include "menu.php";
-?>
+    $id = $_GET['tx'];
+     include ("conexao.php"); 
+
+
+$result = "select * from fornecedor where id = $id";
+        
+        $resoltadobusca = mysqli_query($conn, $result);
+          while($ras = mysqli_fetch_array($resoltadobusca)){
+             
+              
+              
+              $razao = $ras['razao'];
+              $nome_fantasia = $ras['nome_fantasia'];
+              $tipo = $ras['tipo'];
+              $cpf_cnpj = $ras['cpf_cnpj'];
+              $telefone = $ras['telefone'];
+              $email = $ras['email'];
+              $endereco = $ras['endereco'];
+              $bairro = $ras['bairro'];
+              $municipio = $ras['municipio'];
+              $cep = $ras['cep'];
+              
+     
+          
+          }
+
+   
+  
+ ?>
+
+
+
+
 <html>
 	<head>
 		<title>Cadastro de Fornecedor</title>
@@ -11,10 +42,11 @@ include "menu.php";
             
             <div class="container">
             <div class="row">
-                <form action="processa_fornecedor.php" method="GET" >
+                <form action="update_fornecedor.php" method="GET" >
             <fieldset>
                 <div class="panel panel-primary">
                 <div class="panel-heading"><h4>Cadastro de Fornecedores</h4></div>
+                     <input type="text" class="form-control" name="id" value="<?php echo $id; ?>" readonly>
                 <div class="panel-body">
 		<div class="form-inline">
 		        </div>
@@ -22,13 +54,13 @@ include "menu.php";
                 <div class="form-group">
                 <div class="input-group">
                     <span class="input-group-addon">Razão Social:</span>
-                    <input type="text" class="form-control" name="razao">
+                    <input type="text" class="form-control" name="razao" value="<?php echo $razao; ?>">
                 
                  </div>
                 </div>
                 <div class="input-group">
                     <span class="input-group-addon">Nome Fantasia:</span>
-                    <input type="text" class="form-control" name="nome_fantasia">
+                    <input type="text" class="form-control" name="nome_fantasia" value="<?php echo $nome_fantasia; ?>">
                 </div>
                 <br>
                 <div class="form-inline">
@@ -36,47 +68,47 @@ include "menu.php";
                 <div class="input-group">
                     <div class="well">Pessoa:
                         <label class="radio-inline">
-                        <input type="radio" name="tipo" value="F">Fisica
+                        <input type="radio" name="pessoa" value="F" <?php if($tipo == "F") echo "checked" ?>>Fisica
                         </label>
                         <label class="radio-inline">
-                        <input type="radio" name="tipo" value="J">Juridica
+                        <input type="radio" name="pessoa" value="J" <?php if($tipo == "J") echo "checked" ?>>Juridica
                         </label>
                     </div>
                 </div>
                 <div class="input-group">
                     <span class="input-group-addon">CNPJ/CPF:</span>
-                    <input type="text" class="form-control" name="cpf_cnpj">
+                    <input type="text" class="form-control" name="cpf_cnpj" value="<?php echo $cpf_cnpj; ?>">
                 </div></div>
                 <div class="form-inline">
                 <div class="input-group">
                     <span class="input-group-addon">Telefone:</span>
-                    <input type="tel" class="form-control" name="telefone">
+                    <input type="tel" class="form-control" name="telefone" value="<?php echo $telefone; ?>">
                 </div> 
                 <div class="input-group">
                     <span class="input-group-addon">E-mail:</span>
-                    <input type="email" class="form-control" name="email">
+                    <input type="email" class="form-control" name="email" value="<?php echo $email; ?>">
                 </div></div><br>
                 <div class="form-inline">
                 <div class="input-group">
                     <span class="input-group-addon">Endereco:</span>
-                    <input type="text" class="form-control" name="endereco">
+                    <input type="text" class="form-control" name="endereco" value="<?php echo $endereco; ?>">
                 </div>
                 <div class="input-group">
                     <span class="input-group-addon">Numero:</span>
-                    <input type="text" class="form-control" name="numero">
+                    <input type="text" class="form-control" name="numero" value="<?php echo $numero; ?>">
                 </div>
                  <div class="input-group">
                     <span class="input-group-addon">Bairro:</span>
-                    <input type="text" class="form-control" name="bairro">
+                    <input type="text" class="form-control" name="bairro" value="<?php echo $bairro; ?>">
                  </div></div><br>
                 <div class="form-inline">
                 <div class="input-group">
                     <span class="input-group-addon">Municipio:</span>
-                    <input type="text" class="form-control" name="municipio">
+                    <input type="text" class="form-control" name="municipio" value="<?php echo $municipio; ?>">
                 </div>
                 <div class="input-group">
                     <span class="input-group-addon">Cep:</span>
-                    <input type="text" class="form-control" name="cep">
+                    <input type="text" class="form-control" name="cep" value="<?php echo $cep; ?>">
                 </div></div><br><br>
                 
                 <button type="submit"  name="sub" class="btn btn-default">Cadastrar</button>
