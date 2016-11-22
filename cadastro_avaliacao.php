31bf18e40995541f69d865016b99bc64bfbba553
Magno Faustino <magno2004@live.com>
2016-11-22 11:01:19 -0300

    ajustes em cadastro de avaliacao para ficar de acordo com o objetivo

--- cadastro_avaliacao.php
+++ cadastro_avaliacao.php
@@ -5,7 +5,7 @@ include "conexao.php";
 ?>
 <html>
 	<head>
-		<title>Cadastro de Avalicao Fisica</title>
+		<title>Cadastro de Avalição Fisica</title>
 		<meta charset="UTF-8"></meta>
 	</head>
         <body>
@@ -15,12 +15,21 @@ include "conexao.php";
             <form action="processa_avaliacao.php" method="GET" >
             <fieldset>
                 <div class="panel panel-primary">
-                <div class="panel-heading"><h4>Cadastro de Avaliacao Fisica</h4></div>
+                <div class="panel-heading"><h4>Cadastro de Avaliação Fisica</h4></div>
                 <div class="panel-body">
                 
                 <div class="input-group">
                     <span class="input-group-addon">Nome do Aluno:</span>
-                    <input type="text" class="form-control"  title="Preencha o campo Nome" required name="n_aluno">
+                   	     <select class="form-control" title="Aluno ja Cadastrado" required name="n_aluno" >
+                             <option value="">Selecione o Aluno ...</option>
+		   	<?php
+						$result_niveis_acessos = "SELECT * FROM tb_aluno";
+						$resultado_niveis_acesso = mysqli_query($conn, $result_niveis_acessos);
+						while($row_niveis_acessos = mysqli_fetch_assoc($resultado_niveis_acesso)){ ?>
+							<option value="<?php echo $row_niveis_acessos['id']; ?>"><?php echo $row_niveis_acessos['nome']; ?></option> <?php
+						}
+					?>
+                    </select></div><br>
                       </div><br>
                         
                     <div class="input-group">
