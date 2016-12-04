a1d692141348718af3d4c7e90edee82dfec785fd
Magno Faustino <magno2004@live.com>
2016-12-04 20:48:28 -0300

    ainda sem funcionar corretamente

--- login.php
+++ login.php
@@ -1,20 +1,31 @@
 <?php 
-
+include ("conexao.php");
 
   $login = $_POST['login'];
   $entrar = $_POST['entrar'];
   $senha = md5($_POST['senha']);
 
 
-  $connect = mysql_connect('localhost','root','sos101os');
-  $db = mysql_select_db('sisfit');
+
 
 
     if (isset($entrar)) {
+        
             
-      $verifica = mysql_query("SELECT * FROM usuarios WHERE login = '$login' AND senha = '$senha'") or die("erro ao selecionar");
-        if (mysql_num_rows($verifica)<=0){
-          echo"<script language='javascript' type='text/javascript'>alert('Login e/ou senha incorretos');window.location.href='loguin.php';</script>";
+    
+        
+        $result = "select * from usuarios where login = $login or senha = $senha ";
+        
+        
+        $resultadobusca = mysqli_query($conn, $result);
+        
+        
+        
+       if (mysqli_num_rows($resultadobusca)<= 0){
+            
+        //    if($ras = mysqli_fetch_array($resultadobusca)){
+            
+          echo"<script language='javascript'  type='text/javascript'>alert('Login e/ou senha incorretos');window.location.href='login.html';</script>";
           die();
         }else{
           setcookie("login",$login);
