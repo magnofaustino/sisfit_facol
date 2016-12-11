ed4456e73c13879180103632a54fbb714a010f21
Magno Faustino <magno2004@live.com>
2016-12-11 20:10:17 -0300

    login funcionando, lembrando que tem que existir um cadastrado

--- login.php
+++ login.php
@@ -1,36 +1,43 @@
-<?php 
-include ("conexao.php");
+<?php
 
-  $login = $_POST['login'];
-  $entrar = $_POST['entrar'];
-  $senha = md5($_POST['senha']);
+include "conexao.php";
+
+$login = $_POST['login'];
+$senha = $_POST['senha'];
+
+$sql = mysqli_query($conn, "SELECT * FROM usuarios WHERE usuario = '$login'");    
 
 
+while($linha = mysqli_fetch_array($sql))
+{
+    $senha_db = $linha['senha'];
+    $login_db = $linha['usuario'];
+}
 
+$cont = mysqli_num_rows($sql);
 
-
-    if (isset($entrar)) {
-        
-            
-    
-        
-        $result = "select * from usuarios where login = $login or senha = $senha ";
-        
-        
-        $resultadobusca = mysqli_query($conn, $result);
-        
-        
-        
-       if (mysqli_num_rows($resultadobusca)<= 0){
-            
-        //    if($ras = mysqli_fetch_array($resultadobusca)){
-            
-          echo"<script language='javascript'  type='text/javascript'>alert('Login e/ou senha incorretos');window.location.href='login.html';</script>";
-          die();
-        }else{
-          setcookie("login",$login);
-          header("Location:home.php");
-        }
-		
+if($login_db != $login || $login == "")
+{       
+    echo "<meta http-equiv='refresh' content='0; url=login.html'>
+    <script type='text/javascript'>alert('Este usuario não existe')</script>";      
+}
+else
+{
+    if($senha_db != $senha )
+    {
+        echo "<meta http-equiv='refresh' content='0; url=login.html'>
+        <script type='text/javascript'>alert('Senha incorreta')</script>";  
     }
+    else
+    {
+        session_start();
+
+        $_SESSION['login_usuario'] = $login;
+
+        header("location: index.php");  
+    }
+}
+
+mysqli_close($coneccao);
+
 ?>
