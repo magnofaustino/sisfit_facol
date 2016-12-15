6edbdf55775bd598a2e305e1a2b79688864177a2
Magno Faustino <magno2004@live.com>
2016-12-15 10:55:28 -0300

    home_aluno.php

---
+++ home_aluno.php
@@ -0,0 +1,24 @@
+  
+<html>
+	<head>
+		<title>Alunos</title>
+		<meta charset="UTF-8"></meta>
+	</head>
+        <body>
+
+<a href="cadastro_aluno.php" class="btn btn-warning btn-sm" role="button">Segunda</a>
+            
+<a href="cadastro_aluno.php" class="btn btn-warning btn-sm" role="button">Terça</a>
+            
+            <a href="cadastro_aluno.php" class="btn btn-warning btn-sm" role="button">Quarta</a>
+            
+            
+            <a href="cadastro_aluno.php" class="btn btn-warning btn-sm" role="button">Quinta</a>
+            
+            <a href="cadastro_aluno.php" class="btn btn-warning btn-sm" role="button">Sexta</a>
+
+
+            
+                        </body>
+
+</html>
