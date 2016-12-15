cdcc1ce0d96d27820e85af9d4368bdd368584209
Sandro <Sandro@192.168.0.3>
2016-12-15 15:17:54 -0300

    Tela Home

--- home.php
+++ home.php
@@ -4,35 +4,52 @@ include "menu.php";
 ?>
 <html>
 	<head>
-            <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-		<title>Inicio</title>
-                <meta charset="UTF-8"></head>
-        
+        <link href="css/font-awesome.css" rel="stylesheet" type="text/css" />
+		<title>Inicio</title><meta charset="UTF-8">
+	</head>
+ 
+<div class="section">  
 <div class="container">
-      <div class="row">
-        <fieldset>
-        <div class="col-lg-12">
-                <p>
+<fieldset>
+<div class="col-md-12"> 
+<h1 class="text-left" contenteditable="true">Sisfit</h1></div>
+        <div class="col-md-8">
+                <div class="row">
+				<div class="col-md-4">
                     <a href="aluno.php" class="btn btn-sq-lg btn-primary">
-                      <i class="fa fa-user fa-5x"></i><br/>
-                    Cadastro<br>Alunos
-                  </a>
-                    <a href="fornecedores.php" class="btn btn-sq-lg btn-success">
-                    <i class="fa fa-heart fa-5x"></i><br/>
-                    Cadastro<br>Fornecedores
-                  </a>
-                    <a href="atividades.php" class="btn btn-sq-lg btn-warning">
-                    <i class="fa fa-file fa-5x"></i><br/>
-                    Cadastro<br>Atividades
-                  </a>
-                    <a href="produto.php" class="btn btn-sq-lg btn-danger">
-                    <i class="fa fa-star  fa-5x"></i><br/>
-                    Cadastro<br>Produtos
-                  </a>
-                </p>
-        </div>
+                      <i class="glyphicon glyphicon-user fa-12x img-responsive"></i><br/>
+                    <br>Alunos
+                </a></div>
+                <div class="col-md-4">
+                   <a href="atividades.php" class="btn btn-sq-lg btn-success">
+                    <i class="glyphicon glyphicon-list-alt fa-12x img-responsive"></i><br/>
+                    <br>Atividades
+                  </a></div>
+		<div class="col-md-4">
+                    <a href="treino.php" class="btn btn-sq-lg btn-success">
+                    <i class="glyphicon glyphicon-flash fa-12x img-responsive"></i><br/>
+                    <br>Treinos
+                 </a></div>
+		</div><br>
+		<div class="row">
+			<div class="col-md-4">
+                        <a href="avaliacoes.php" class="btn btn-sq-lg btn-warning">
+                    <i class="glyphicon glyphicon-pencil fa-12x img-responsive"></i><br/>
+                    <br>Avaliação
+                  </a></div>
+			<div class="col-md-4">
+                        <a href="perimetro.php" class="btn btn-sq-lg btn-danger">
+                    <i class="glyphicon glyphicon-indent-left fa-12x img-responsive"></i><br/>
+                    <br>Perímetro
+                  </a></div>
+			<div class="col-md-4">
+                        <a href="tela_aniversariantes.php" class="btn btn-sq-lg btn-success">
+                    <i class="glyphicon glyphicon-gift fa-12x img-responsive"></i><br/>
+                    <br>Aniversariantes do Mês
+                  </a></div>
+		 		  </div>
+				  </div>
         </fieldset>
-      </div>
-</div>    
+</div></div>    
 </html>
             
---
+++ home3.php
@@ -0,0 +1,38 @@
+<?php
+
+include "menu.php";
+?>
+<html><head>
+
+    </head><body>
+
+<div class="section"> 
+<div class="container"> 
+<div class="row"> 
+<fieldset>
+<div class="col-md-12"> 
+<h1 class="text-center" contenteditable="true">Sisfit</h1> </div>
+</div><div class="row"> 
+<div class="col-md-4"> 
+<a href="aluno.php"><img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive"></a> 
+<h2 class="text-danger">Alunos</h2> 
+</div><div class="col-md-4"> 
+<a><img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive"></a> 
+<h2 class="text-warning">Atividades</h2> 
+</div>
+<div class="col-md-4"> 
+<a><img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive"></a> 
+<h2 class="text-success">Treinos</h2> </div></div><div class="row"> 
+<div class="col-md-4"> 
+<a><img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive"></a> 
+<h2 class="text-info">Avaliação</h2> 
+</div><div class="col-md-4"> 
+<a><img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive"></a> 
+<h2 class="text-primary">Perímetro</h2> 
+</div><div class="col-md-4"> 
+<a><img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png" class="img-responsive"></a> 
+<h2 class="text-muted">Aniversariantes do Mês</h2> 
+</div>
+<fieldset>
+</div></div></div>
+</body></html>
